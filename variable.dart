6224e55f2b7830b9dca0462db670682653f21662
Hasnain-Developer1 <hasnainmalik9925@gmail.com>
2026-08-05 02:20:24 +0500

    Variables in Dart

---
+++ variable.dart
@@ -0,0 +1,19 @@
+void main(){
+  print("This is my first Dart Program!");
+  int price = 1000;
+  double cgpa = 3.3;
+  String username = "Ali";
+  String lastname = 'Ahmad';
+
+  var test = "100.00"; // We can store any type of data in var
+  print("My product price = $price");
+
+  int num1 = 100;
+  int num2 = 45;
+
+  int add = num1 + num2;
+  int mul = num1 * num2;
+
+  print("Sum of two number is $add");
+  print("Multiplication of two number is $mul");
+}
