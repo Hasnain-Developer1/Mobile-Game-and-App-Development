59019403ee54691450363b9b0da3ead3aa221db4
Hasnain-Developer1 <hasnainmalik9925@gmail.com>
2026-08-17 22:32:48 -0700

    Container Widget in Flutter

---
+++ project_13/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:project_12/screens/dashboard_screen.dart';
+import 'package:project_12/screens/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: DashboardScreen(), // start with dashboard screen like Home_screen or dashboard_screen
+  ));
+}
+
+
