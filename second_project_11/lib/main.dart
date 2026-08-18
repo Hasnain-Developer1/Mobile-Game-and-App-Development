a78e0f976529920d46767101b90dbbb9d4adb609
Hasnain-Developer1 <hasnainmalik9925@gmail.com>
2026-08-17 19:58:49 -0700

    Second Testing Project and connecting flutter to physical devices

---
+++ second_project_11/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text("I am testing my Flutter APP"),
+      ),
+      theme: ThemeData(),
+    )
+  );
+}
