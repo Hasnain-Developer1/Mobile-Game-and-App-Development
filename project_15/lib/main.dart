9a8f3b9c10414ad6306621936bd1f772c4a5068e
Hasnain-Developer1 <hasnainmalik9925@gmail.com>
2026-08-18 05:26:24 -0700

    Main Dart File

---
+++ project_15/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+// import 'package:project_12/screens/dashboard_screen.dart';
+// import 'package:project_12/screens/home_screen.dart';
+// import 'package:project_12/screens/row_screen.dart';
+import 'package:project_12/screens/column_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: ColumnScreen(),
+  ));
+}
+
+
