554e673b3910ef67d83a7f0fcf4b73a867941741
Hasnain-Developer1 <hasnainmalik9925@gmail.com>
2026-08-19 01:25:31 -0700

    Main Dart File

---
+++ project_16/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+// import 'package:project_12/screens/dashboard_screen.dart';
+// import 'package:project_12/screens/home_screen.dart';
+// import 'package:project_12/screens/row_screen.dart';
+import 'package:project_12/screens/column_screen.dart';
+import 'package:project_12/screens/padding_sizedbox.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: ColumnScreen(),
+  ));
+}
+
+
