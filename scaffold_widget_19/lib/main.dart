e70b6f2202f9ce1addb488f69dcab26bbe80a72c
Hasnain-Developer1 <hasnainmalik9925@gmail.com>
2026-08-20 00:08:32 -0700

    Main Dart File

---
+++ scaffold_widget_19/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:project_12/screens/buttonscreen.dart';
+// import 'package:project_12/screens/dashboard_screen.dart';
+// import 'package:project_12/screens/home_screen.dart';
+// import 'package:project_12/screens/row_screen.dart';
+import 'package:project_12/screens/column_screen.dart';
+import 'package:project_12/screens/home_screen.dart';
+import 'package:project_12/screens/padding_sizedbox.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: ProfileScreen(),
+  ));
+}
+
+
